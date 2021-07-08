1a85899ebe88a661741d1881a76870c457f7dcdd
UnMondodiBonobo <alessandro.profera@gmail.com>
2021-07-08 17:15:01 +0200

    GameOfLife Cells

---
+++ source/Cell.cpp
@@ -0,0 +1,198 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Cell.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "DrawDebugHelpers.h"
+
+// Sets default values
+ACell::ACell()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	SetRootComponent(Mesh);
+
+	Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("Collider"));
+	Collider->SetupAttachment(Mesh);
+
+	
+
+}
+
+// Called when the game starts or when spawned
+void ACell::BeginPlay()
+{
+	Super::BeginPlay();
+	SetActorTickEnabled(false);
+	
+	/* At the first iteration check if the cell is set to dead or alive and change the material*/
+	bIsDead == true ? Mesh->SetMaterial(0, DeadMaterial) : Mesh->SetMaterial(0, AliveMaterial);
+	bIsDeadtemp = bIsDead;
+
+	/*Sets the timer that loops through the iteration*/
+	GetWorldTimerManager().SetTimer(TimerHandleIteration, this,  &ACell::Interaction,0.1f, true);
+	
+}
+
+/* This function checks all the cell's neighbours, counts the living ones and the dead ones and sets bIsDeadtemp following the rules*/
+void ACell::Interaction() 
+{
+	
+	Dead = 0;
+	Alive = 0;
+
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(this);
+
+	/* Calculate all vectors around (up, down, right , left and diagonals) and always use a correct vector length based on trigonometric standards*/
+	Start = GetActorLocation();
+	EndRight = Start+FVector(0.f, Radius, 0.f);
+	EndLeft = Start+FVector(0.f, -Radius, 0.f);
+	EndUp = Start+FVector(0.f, 0.f, Radius);
+	EndDown = Start+FVector(0.f, 0.f, -Radius);
+	EndRightUp = Start+FVector(0.f, Radius*DiagonalAngle, Radius*DiagonalAngle);
+	EndLeftUp = Start+FVector(0.f, -Radius*DiagonalAngle, Radius*DiagonalAngle);
+	EndRightDown = Start+FVector(0.f, Radius*DiagonalAngle, -Radius*DiagonalAngle);
+	EndLeftDown = Start+FVector(0.f, -Radius*DiagonalAngle, -Radius*DiagonalAngle);
+
+
+	/*Render a red line for each vector, check his neighbours*/
+
+	IsHitRight = GetWorld()->LineTraceSingleByChannel(HitRight, Start, EndRight, ECollisionChannel::ECC_Visibility, QueryParams);
+	//DrawDebugLine(GetWorld(), Start, EndRight, FColor::Red, false, 0.4f);
+
+	IsHitLeft = GetWorld()->LineTraceSingleByChannel(HitLeft, Start, EndLeft, ECollisionChannel::ECC_Visibility, QueryParams);
+	//DrawDebugLine(GetWorld(), Start, EndLeft, FColor::Red, false, 0.4f);
+
+	IsHitUp = GetWorld()->LineTraceSingleByChannel(HitUp, Start, EndUp, ECollisionChannel::ECC_Visibility, QueryParams);
+	//DrawDebugLine(GetWorld(), Start, EndUp, FColor::Red, false, 0.4f);
+
+	IsHitDown = GetWorld()->LineTraceSingleByChannel(HitDown, Start, EndDown, ECollisionChannel::ECC_Visibility, QueryParams);
+	//DrawDebugLine(GetWorld(), Start, EndDown, FColor::Red, false, 0.4f);
+
+	IsHitRightUp = GetWorld()->LineTraceSingleByChannel(HitRightUp, Start, EndRightUp, ECollisionChannel::ECC_Visibility, QueryParams);
+	//DrawDebugLine(GetWorld(), Start, EndRightUp, FColor::Red, false, 0.4f);
+
+	IsHitLeftUp = GetWorld()->LineTraceSingleByChannel(HitLeftUp, Start, EndLeftUp, ECollisionChannel::ECC_Visibility, QueryParams);
+	//DrawDebugLine(GetWorld(), Start, EndLeftUp, FColor::Red, false, 0.4f);
+
+	IsHitRightDown = GetWorld()->LineTraceSingleByChannel(HitRightDown, Start, EndRightDown, ECollisionChannel::ECC_Visibility, QueryParams);
+	//DrawDebugLine(GetWorld(), Start, EndRightDown, FColor::Red, false, 0.4f);
+
+	IsHitLeftDown = GetWorld()->LineTraceSingleByChannel(HitLeftDown, Start, EndLeftDown, ECollisionChannel::ECC_Visibility, QueryParams);
+	//DrawDebugLine(GetWorld(), Start, EndLeftDown, FColor::Red, false, 0.4f);
+
+	if(IsHitDown && HitDown.GetActor() != this) 
+	{
+		CellDown = Cast<ACell>(HitDown.GetActor());
+		if(CellDown->bIsDead == true){Dead++;}  
+		else{Alive++;}
+
+		
+	}
+	if(IsHitUp && HitUp.GetActor() != this) 
+	{
+		CellUp = Cast<ACell>(HitUp.GetActor());
+		if(CellUp->bIsDead == true){Dead++;}  
+		else{Alive++;}
+		
+		
+	}	
+	if(IsHitRight && HitRight.GetActor() != this) 
+	{
+		
+		CellRight = Cast<ACell>(HitRight.GetActor());
+		if(CellRight->bIsDead == true){Dead++;}  
+		else{Alive++;}
+		
+		
+	}
+	
+	if(IsHitLeft && HitLeft.GetActor() != this) 
+	{
+		CellLeft = Cast<ACell>(HitLeft.GetActor());
+		if(CellLeft->bIsDead == true){Dead++;}  
+		else{Alive++;}
+				
+	
+	}
+
+	if(IsHitRightUp && HitRightUp.GetActor() != this) 
+	{
+		CellRightUp = Cast<ACell>(HitRightUp.GetActor());
+		if(CellRightUp->bIsDead == true){Dead++;}  
+		else{Alive++;}
+		
+		
+	}
+
+	if(IsHitRightDown && HitRightDown.GetActor() != this) 
+	{
+		CellRightDown = Cast<ACell>(HitRightDown.GetActor());
+		if(CellRightDown->bIsDead == true){Dead++;}  
+		else{Alive++;}
+	
+	}
+	
+	if(IsHitLeftUp && HitLeftUp.GetActor() != this) 
+	{
+
+		CellLeftUp = Cast<ACell>(HitLeftUp.GetActor());
+		if(CellLeftUp->bIsDead == true){Dead++;}  
+		else{Alive++;}
+
+	
+	}
+
+	if(IsHitLeftDown && HitLeftDown.GetActor() != this) 
+	{
+		CellLeftDown = Cast<ACell>(HitLeftDown.GetActor());
+		if(CellLeftDown->bIsDead == true){Dead++;}  
+		else{Alive++;}
+
+		
+	}
+
+	if(bIsDead)/*If this cell is already dead*/
+	{
+		/*Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.*/
+
+		if(Alive == 3) {bIsDeadtemp = false;}
+	}
+
+	else /* if this cell is still alive*/
+	{	
+		/*Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+		Any live cell with more than three live neighbours dies, as if by overpopulation.*/
+		
+		if(Alive < 2 || Alive > 3) {bIsDeadtemp = true; }
+	}
+
+	/* Call ChangeState function after 0.05 seconds Interaction fucntion has been called*/
+	GetWorldTimerManager().SetTimer(TimerHandleToChangeState, this, &ACell::ChangeState, 0.05f, false);
+
+
+}
+
+/*This function change the state of the cells, based on the result of bIsDeadtemp*/
+void ACell::ChangeState() 
+{
+	bIsDead = bIsDeadtemp;
+	bIsDead == true ? Mesh->SetMaterial(0, DeadMaterial) : Mesh->SetMaterial(0, AliveMaterial);
+	GetWorldTimerManager().ClearTimer(TimerHandleToChangeState);
+}
+
+// Called every frame
+void ACell::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+
+	
+
+}
+
---
+++ source/Cell.h
@@ -0,0 +1,146 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Cell.generated.h"
+
+class UStaticMeshComponent;
+class UBoxComponent;
+
+UCLASS()
+class GAMEOFLIFE_API ACell : public AActor
+{
+	GENERATED_BODY()
+	
+	public:	
+		// Sets default values for this actor's properties
+		ACell();
+
+		//Create Mesh component and collider component
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components", meta=(AllowPrivateAccess = true))
+		UStaticMeshComponent* Mesh;
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components", meta=(AllowPrivateAccess = true))
+		UBoxComponent* Collider;
+
+		/* Create boolean variable to check if it's Dead or Alive */
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DeadOrAlive", meta=(AllowPrivateAccess = true))
+		bool bIsDead;
+
+		UPROPERTY(EditAnywhere)
+		float Radius = 200.f;
+
+		/* 45° degrees variation angle*/
+		UPROPERTY(VisibleAnywhere)
+		double DiagonalAngle = PI/4;
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UMaterialInterface* DeadMaterial;
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UMaterialInterface* AliveMaterial;
+
+
+		// Called every frame
+		virtual void Tick(float DeltaTime) override;
+
+	protected:
+		// Called when the game starts or when spawned
+		virtual void BeginPlay() override;
+
+	private:
+		FTimerHandle TimerHandleIteration;
+		FTimerHandle TimerHandleToChangeState;
+
+		/* These values are declared here in the .h file so we don't have to recreate them each iteration and UPROPERTY MACRO can handle potential memory leaks  */
+		UPROPERTY()
+		FHitResult HitRight;
+		UPROPERTY()
+		FHitResult HitLeft;
+		UPROPERTY()
+		FHitResult HitUp;
+		UPROPERTY()
+		FHitResult HitDown;
+		UPROPERTY()
+		FHitResult HitRightUp;
+		UPROPERTY()
+		FHitResult HitRightDown;
+		UPROPERTY()
+		FHitResult HitLeftUp;
+		UPROPERTY()
+		FHitResult HitLeftDown;
+
+		UPROPERTY()
+		FVector Start;
+		UPROPERTY()
+		FVector EndRight;
+		UPROPERTY()
+		FVector EndLeft; 
+		UPROPERTY()
+		FVector EndUp ;
+		UPROPERTY()
+		FVector EndDown ;
+		UPROPERTY()
+		FVector EndRightUp; 
+		UPROPERTY()
+		FVector EndLeftUp;
+		UPROPERTY()
+		FVector EndRightDown; 
+		UPROPERTY()
+		FVector EndLeftDown;
+
+		UPROPERTY()
+		bool IsHitRight;
+		UPROPERTY()
+		bool IsHitLeft;
+		UPROPERTY()
+		bool IsHitUp;
+		UPROPERTY()
+		bool IsHitDown;
+		UPROPERTY()
+		bool IsHitRightUp;
+		UPROPERTY()
+		bool IsHitLeftUp;
+		UPROPERTY()
+		bool IsHitRightDown;
+		UPROPERTY()
+		bool IsHitLeftDown;
+
+
+		UFUNCTION()
+		void Interaction();
+
+		UFUNCTION()
+		void ChangeState();
+
+		UPROPERTY()
+		TArray<ACell*> Neighbours;
+
+		int32 Dead = 0;
+		int32 Alive = 0;
+
+		UPROPERTY()
+		ACell* CellDown;
+		UPROPERTY()
+		ACell* CellUp;
+		UPROPERTY()
+		ACell* CellRight;
+		UPROPERTY()
+		ACell* CellLeft;
+		UPROPERTY()
+		ACell* CellRightUp;
+		UPROPERTY()
+		ACell* CellRightDown;
+		UPROPERTY()
+		ACell* CellLeftUp;
+		UPROPERTY()
+		ACell* CellLeftDown;
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "DeadOrAlive", meta=(AllowPrivateAccess = true))
+		bool bIsDeadtemp;
+
+
+	
+};
